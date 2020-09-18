c791f00c600edda9a2fc4598042eb47240e8bf06
Kevinclane <kevinclane21@gmail.com>
2020-09-18 00:08:38 -0600

    eidts

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -28,77 +28,268 @@
       </div>
       <div class="row my-2 d-flex justify-content-around">
         <h3 class="col-12 text-center">Browse our catagories</h3>
-        <div class="col-md-3 col-10 m-3">
-          <div class="card boxShadowCstm cardHeight">
-            <img
-              src="../assets/errandsFour.jpg"
-              class="card-img-top cardImg"
-              alt="error loading image"
-            />
-            <div class="card-body d-flex align-items-end justify-content-center">
-              <h5 class="card-title">Errands/Small Tasks</h5>
+        <div class="col-10 offset-1">
+          <div class="row">
+            <div class="col-md-3 col-10 m-3">
+              <div class="card boxShadowCstm cardHeight">
+                <img
+                  src="../assets/errandsFour.jpg"
+                  class="card-img-top cardImg"
+                  alt="error loading image"
+                />
+                <div class="card-body d-flex align-items-end justify-content-center">
+                  <h5 class="card-title">Errands/Small Tasks</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-10 m-3">
+              <div class="card boxShadowCstm cardHeight">
+                <img
+                  src="../assets/cleaningSeven.jpg"
+                  class="card-img-top cardImg"
+                  alt="error loading image"
+                />
+                <div class="card-body d-flex align-items-end justify-content-center">
+                  <h5 class="card-title">Household Chores</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-10 m-3">
+              <div class="card boxShadowCstm cardHeight">
+                <img
+                  src="../assets/mechanicTwo.jpg"
+                  class="card-img-top cardImg"
+                  alt="error loading image"
+                />
+                <div class="card-body d-flex align-items-end justify-content-center">
+                  <h5 class="card-title">Automotive Mechanic</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-10 m-3">
+              <div class="card boxShadowCstm cardHeight">
+                <img
+                  src="../assets/contractorOne.jpg"
+                  class="card-img-top cardImg"
+                  alt="error loading image"
+                />
+                <div class="card-body d-flex align-items-end justify-content-center">
+                  <h5 class="card-title">Contractor</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-10 m-3">
+              <div class="card boxShadowCstm cardHeight">
+                <img
+                  src="../assets/babysitterOne.jpg"
+                  class="card-img-top cardImg"
+                  alt="error loading image"
+                />
+                <div class="card-body d-flex align-items-end justify-content-center">
+                  <h5 class="card-title">Babysitter</h5>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3 col-10 m-3">
+              <div class="card boxShadowCstm cardHeight">
+                <img
+                  src="../assets/otherOne.jpg"
+                  class="card-img-top cardImg"
+                  alt="error loading image"
+                />
+                <div class="card-body d-flex align-items-end justify-content-center">
+                  <h5 class="card-title">Other Services</h5>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-10 m-3">
-          <div class="card boxShadowCstm cardHeight">
-            <img
-              src="../assets/cleaningSeven.jpg"
-              class="card-img-top cardImg"
-              alt="error loading image"
-            />
-            <div class="card-body d-flex align-items-end justify-content-center">
-              <h5 class="card-title">Household Chores</h5>
+      </div>
+      <div class="row bg-white">
+        <div class="col-10 offset-1 p-3">
+          <h3>Looking for a service</h3>
+          <h5>(car repair, house painting, babysitting) – but not sure how to find qualified help? Post your wish. Don’t get sold. Don’t get up-sold. Pay only if the service is delivered to scope</h5>
+          <div class="row">
+            <div class="col-12 col-md-6 offset-md-3 d-flex justify-content-center">
+              <button
+                class="btn btn-success btn-lg w-100 rounded-button-lg boxShadowCstm"
+              >I need some help</button>
+            </div>
+          </div>
+          <div class="row text-center my-3">
+            <div class="col-12 boxShadowCstm">Featured services</div>
+          </div>
+        </div>
+        <div class="col-10 offset-1 p-3">
+          <h3>A contractor?</h3>
+          <h5>(Aspiring contractor or simply a good neighbor looking to help?) Get FREE LEADS, or ADVERTISE your best offer — post it FREE. ALWAYS get paid (you won’t have to chase your check)</h5>
+          <div class="row">
+            <div class="col-12 col-md-6 offset-md-3 d-flex justify-content-center">
+              <button
+                class="btn btn-success btn-lg w-100 rounded-button-lg boxShadowCstm"
+              >I'm looking for jobs</button>
+            </div>
+          </div>
+          <div class="row text-center my-3">
+            <div class="col-12 boxShadowCstm">Featured jobs</div>
+          </div>
+        </div>
+      </div>
+      <div class="row py-2">
+        <div class="col-10 offset-1 col-md-3 offset-md-2 d-flex justify-content-center">
+          <img src="../assets/laundry.jpeg" alt="error loading image" />
+        </div>
+        <div class="col-10 offset-1 col-md-4">
+          <div class="row">
+            <div class="col-12">
+              <h3>We Have</h3>
+              <ul>
+                <li>
+                  <span class="bold">A live auction</span> for every project!
+                </li>
+                <li>
+                  <span class="bold">Total transparancy</span>of price, scope, and all bidders' credentials!
+                </li>
+                <li>
+                  <span class="bold">Escrow service --</span> great payment protection (and payment guarantee!)
+                </li>
+                <li>
+                  <span class="bold">Free</span> dispute resolution!
+                </li>
+                <li>
+                  <span class="bold">Largest databasees</span> of service providers to research (and be a member of)!
+                </li>
+                <li>
+                  <span class="bold">A great way to offer (and find!) discounts</span>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-10 m-3">
-          <div class="card boxShadowCstm cardHeight">
-            <img
-              src="../assets/mechanicTwo.jpg"
-              class="card-img-top cardImg"
-              alt="error loading image"
-            />
-            <div class="card-body d-flex align-items-end justify-content-center">
-              <h5 class="card-title">Automotive Mechanic</h5>
+      </div>
+      <div class="row d-flex justify-content-center py-3">
+        <div class="col-10 col-md-5 my-2">
+          <button
+            class="btn btn-success btn-lg w-100 rounded-button-lg boxShadowCstm"
+          >I need some help</button>
+        </div>
+        <div class="col-10 col-md-5 my-2">
+          <button
+            class="btn btn-success btn-lg w-100 rounded-button-lg boxShadowCstm"
+          >I'm looking for jobs</button>
+        </div>
+      </div>
+      <div class="row bg-white py-2">
+        <div class="col-10 offset-1">
+          <h3>Elves and Errands is a magic site that helps bring businesses and clients together. It will come handy when:</h3>
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <ul>
+                <li>You want a good job at a reasonable price</li>
+                <li>You want to be able to evaluate offers and compare prices BEFORE talking to a salesperson</li>
+                <li>You are looking for a good deal</li>
+                <li>You want community feedback on how good a provider actually is</li>
+                <li>You are looking for help with a simple, small job and don’t need to hear big companies’ expensive sales pitches</li>
+                <li>You need help with a task that is too small for a business: like, making a grocery store run</li>
+                <li>You need something done TONIGHT</li>
+                <li>YOU DON’T WANT TO PAY A FEE TO ACHIEVE ANY OF THE ABOVE</li>
+              </ul>
+            </div>
+            <div class="col-12 col-md-6">
+              <ul>
+                <li>You are looking for the best place to offer your services</li>
+                <li>You want FREE LEADS</li>
+                <li>You want to post your advertisement FREE OF CHARGE</li>
+                <li>You want to be able post your offer or coupon and have it reach thousands of people in your local community—at no expense to you</li>
+                <li>You want to avoid the risk of delivering a good job and still not getting paid</li>
+                <li>You are looking for a side job</li>
+                <li>You are new in business and do not have a huge advertising budget</li>
+              </ul>
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-10 m-3">
-          <div class="card boxShadowCstm cardHeight">
-            <img
-              src="../assets/contractorOne.jpg"
-              class="card-img-top cardImg"
-              alt="error loading image"
-            />
-            <div class="card-body d-flex align-items-end justify-content-center">
-              <h5 class="card-title">Contractor</h5>
+      </div>
+      <div class="row d-flex justify-content-center">
+        <div class="col-10 text-center my-2">
+          <h3>How it works</h3>
+          <h5>Hint:</h5>
+          <h4>All of this is Free!</h4>
+        </div>
+        <div class="col-10 vidBg p-4">
+          <div class="row">
+            <div class="col-4">
+              <h3 class="text-red">Overwhelmed?</h3>
+              <h3 class="text-red">Little time for yourself?</h3>
+              <h4 class="text-center text-green my-5">
+                Or simply looking for
+                <span class="text-red underline">professional help with</span> your home projects?
+              </h4>
+              <h5 class="text-center">Take the decision to delegate — and we’ll do the rest.</h5>
+              <h5 class="text-white text-center">(Best part: It’s FREE!)</h5>
+            </div>
+            <div class="col-8 d-flex justify-content-center align-items-center">
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/cDQt5x0aZB4"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-10 m-3">
-          <div class="card boxShadowCstm cardHeight">
-            <img
-              src="../assets/babysitterOne.jpg"
-              class="card-img-top cardImg"
-              alt="error loading image"
-            />
-            <div class="card-body d-flex align-items-end justify-content-center">
-              <h5 class="card-title">Babysitter</h5>
+        <div class="col-12 my-4 bg-white">
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-1">
+              <i class="fa fa-check fa-2x text-success mr-2" aria-hidden="true"></i>
+            </div>
+            <div class="col-8">
+              <h5>
+                <b>Need something done –</b>but not sure where to find the right person for the job? Search pre-made offers(eCoupons) or Speak your wish and post your request
+              </h5>
+            </div>
+          </div>
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-1">
+              <i class="fa fa-check fa-2x text-success mr-2" aria-hidden="true"></i>
+            </div>
+            <div class="col-8">
+              <h5>
+                <b>Quality-checked bidders</b> (with reviews!) will give you their offers (eCoupons) in response to your request.
+              </h5>
+            </div>
+          </div>
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-1">
+              <i class="fa fa-check fa-2x text-success mr-2" aria-hidden="true"></i>
+            </div>
+            <div class="col-8">
+              <h5>
+                <b>When you get your offers…</b> Take them or leave them! (This is how you can check the price and quality on different offers without feeling committed to the first person you speak with)
+              </h5>
+            </div>
+          </div>
+          <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-1">
+              <i class="fa fa-check fa-2x text-success mr-2" aria-hidden="true"></i>
+            </div>
+            <div class="col-8">
+              <h5>
+                <b>Your price and service are GUARANTEED on these eCoupons.</b>You do not BUY services on this websiteYou PUT YOUR MONEY into an escrow, as a pledge of payment. If the service is completed to your satisfaction, the escrow is released to the service provider. If not – back to you. If you disagree – request a resolution. The Dispute services are free.
+              </h5>
             </div>
           </div>
         </div>
-        <div class="col-md-3 col-10 m-3">
-          <div class="card boxShadowCstm cardHeight">
-            <img
-              src="../assets/otherOne.jpg"
-              class="card-img-top cardImg"
-              alt="error loading image"
-            />
-            <div class="card-body d-flex align-items-end justify-content-center">
-              <h5 class="card-title">Other Services</h5>
-            </div>
-          </div>
+        <div class="col-10 col-md-5 my-2">
+          <button
+            class="btn btn-success btn-lg w-100 rounded-button-lg boxShadowCstm"
+          >I need some help</button>
+        </div>
+        <div class="col-10 col-md-5 my-2">
+          <button
+            class="btn btn-success btn-lg w-100 rounded-button-lg boxShadowCstm"
+          >I'm looking for jobs</button>
         </div>
       </div>
     </div>
@@ -127,6 +318,17 @@ export default {
   user-select: none;
   height: 50vh;
 }
+.vidBg {
+  background: url(../assets/videoBg.png);
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  margin: 0 auto;
+  overflow-x: hidden;
+  user-select: none;
+  background-position: center;
+}
 .fontCabin {
   font-family: "Cabin", sans-serif;
 }
@@ -154,6 +356,31 @@ export default {
   height: 40vh;
 }
 .mainImg {
-  max-width: 90vw;
+  max-width: 100%;
+}
+.w-40 {
+  width: 40%;
+}
+.bold {
+  font-weight: bold;
+}
+li {
+  font-size: 1.25rem;
+}
+.fontUp {
+  font-style: 1.25rem;
+}
+.text-green {
+  color: #008000;
+}
+.text-red {
+  color: #800000;
+}
+.underline {
+  text-decoration: underline;
+}
+.video {
+  width: 100px;
+  height: 100px;
 }
 </style>
